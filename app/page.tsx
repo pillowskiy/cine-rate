6cecc0d0fdcc3103b227ffb5ef6fb8da334c1e58
Pillow <fagodfagod@gmail.com>
2023-08-13 18:48:43 +0300

    components -> upcoming creations

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,15 @@ import { ScrollArea, ScrollBar } from '@ui/scroll-area';
 import { getTrending } from '@actions/getTrending';
 import { getCelebrities } from '@actions/getCelebrities';
 import { CelebrityCard } from '@components/celebrity-card';
+import { getUpcoming } from '@actions/getUpcoming';
+import { CreationHoverCard } from '@components/creations/creation-hover-card';
 
 export default async function Home() {
+  // TEMP
   const { data: creations } = await getTrending({});
   const { data: celebrities } = await getCelebrities({});
-  if (!creations || !celebrities) return null;
+  const { data: upcoming } = await getUpcoming({});
+  if (!creations || !celebrities || !upcoming) return null;
 
   return (
     <div className='h-screen w-full'>
@@ -49,6 +53,31 @@ export default async function Home() {
         <section className='flex snap-x space-x-4 pb-4'>
           {creations.results.map((creation) => (
             <CreationCard
+              key={creation.id}
+              creation={creation}
+              className='w-[260px]'
+              size='sm'
+              width={720}
+              height={480}
+            />
+          ))}
+        </section>
+        <ScrollBar className='' orientation='horizontal' />
+      </ScrollArea>
+
+      <div className='mt-6 flex items-center justify-between'>
+        <div className='space-y-1'>
+          <h2 className='text-2xl font-semibold tracking-tight'>Upcoming!</h2>
+          <p className='text-sm text-muted-foreground'>
+            Novelties that may be interesting
+          </p>
+        </div>
+      </div>
+      <Separator className='my-4' />
+      <ScrollArea>
+        <section className='flex snap-x space-x-4 pb-4'>
+          {upcoming.results.map((creation) => (
+            <CreationHoverCard
               key={creation.id}
               creation={creation}
               className='w-[260px]'
