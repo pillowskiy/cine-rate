587415de6f02a14405169f49011ec9d5227cfe42
Pillow <fagodfagod@gmail.com>
2023-08-20 20:00:16 +0300

    chore -> youtube-player -> update player options

--- app/_components/youtube-player.tsx
+++ app/_components/youtube-player.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react';
 import { cn } from '../_libs';
 import ReactPlayer, { ReactPlayerProps } from 'react-player';
-import { VolumeX, Volume2, Youtube } from 'lucide-react';
+import { VolumeX, Volume2, Youtube, Play, Divide } from 'lucide-react';
 import { Button } from './ui/button';
 import Link from 'next/link';
 
@@ -17,17 +17,23 @@ export function YoutubePlayer({
   className,
   ...props
 }: YoutubePlayerProps) {
-  const [muted, setMuted] = useState(true);
+  const [isMuted, setIsMuted] = useState(true);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   return (
-    <figure className={cn('relative overflow-hidden rounded-md grid place-items-center', className)}>
+    <figure
+      className={cn(
+        'relative grid place-items-center overflow-hidden rounded-md',
+        className
+      )}
+    >
       <div className='absolute right-2 top-2 flex flex-col gap-2 md:right-4 md:top-4'>
         <Button
           className='h-7 w-7 md:h-10 md:w-10'
           size='icon'
-          onClick={() => setMuted((prev) => !prev)}
+          onClick={() => setIsMuted((prev) => !prev)}
         >
-          {muted ? (
+          {isMuted ? (
             <VolumeX className='h-5 w-5' />
           ) : (
             <Volume2 className='h-5 w-5' />
@@ -41,28 +47,31 @@ export function YoutubePlayer({
       </div>
       <ReactPlayer
         url={url}
-        className='pointer-events-none w-full'
+        className='w-full'
         width='120%'
         height='130%'
-        muted={muted}
+        muted={isMuted}
+        playing={isPlaying}
         loop
+        playIcon={<Play className='h-7 w-8 fill-primary-foreground' />}
+        stopOnUnmount
         config={{
           youtube: {
             playerVars: {
               modestbranding: 1,
               disablekb: 1,
               controls: 0,
+              autohide: 1,
               // autoplay: 1,
               iv_load_policy: 3,
               cc_load_policy: 3,
               fs: 0,
               showinfo: 0,
               showsearch: 0,
-              rel: 1,
+              rel: 0,
             },
           },
         }}
-        playing
         {...props}
       />
     </figure>
