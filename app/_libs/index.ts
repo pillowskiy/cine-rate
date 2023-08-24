9d324073bed284c1d89dc5ebb762c98bde467f8e
Pillow <fagodfagod@gmail.com>
2023-08-24 15:40:31 +0300

    libs -> common -> capitalize util

--- app/_libs/index.ts
+++ app/_libs/index.ts
@@ -1,6 +1,20 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+interface CapitalizeOptions {
+  join?: string;
+  assignLowerCase?: boolean;
+}
+export function capitalize(
+  string: string,
+  options?: CapitalizeOptions
+): string {
+  return (options?.assignLowerCase ? string.toLocaleLowerCase() : string)
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(options?.join || ' ');
 }
