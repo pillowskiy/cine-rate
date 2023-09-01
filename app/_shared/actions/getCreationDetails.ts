2df15ac9fe838c547ad1817e441823e6f6a64e8a
Pillow <fagodfagod@gmail.com>
2023-09-01 19:13:18 +0300

    actions -> creation details

--- app/_shared/actions/getCreationDetails.ts
+++ app/_shared/actions/getCreationDetails.ts
@@ -1,13 +1,17 @@
 import type { IMovieDetails } from '@app/types/movies-types';
+import type { ITVDetails } from '@app/types/tv-types';
 import type { BaseParams, MediaType } from '@app/types/index';
 import { $api } from '../api/api-interceptor';
 
-export const getCreationDetails = (
+export function getCreationDetails<T extends MediaType>(
   creationId: number,
-  mediaType: MediaType,
+  mediaType: T,
   params?: BaseParams
-) => {
-  return $api.get<IMovieDetails>(`/3/${mediaType}/${creationId}`, {
-    params,
-  });
-};
+) {
+  return $api.get<T extends MediaType.Movie ? IMovieDetails : ITVDetails>(
+    `/3/${mediaType}/${creationId}`,
+    {
+      params,
+    }
+  );
+}
---
+++ app/_types/tv-types.ts
@@ -0,0 +1,76 @@
+import { ICreationLanguage, IProductionCountry } from '.';
+import { IPartialCompany } from './company-types';
+import type { IPartialGenre } from './genre-types';
+import { ICelebrity } from './person-types';
+
+export interface ITVDetails {
+  created_by: ICelebrity[];
+  episode_run_time: number[];
+  first_air_date: string;
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: null | number;
+  budget: number;
+  genres: IPartialGenre[];
+  homepage?: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: IPartialCompany[];
+  production_countries: IProductionCountry[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: ICreationLanguage[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+
+  in_production: boolean;
+  languages: string[];
+  last_air_date: string;
+  last_episode_to_air: IEpisode | null;
+  name: string;
+  next_episode_to_air: IEpisode | null;
+  networks: unknown;
+  number_of_episodes: number;
+  number_of_seasons: number;
+  origin_country: string[];
+  original_name: string;
+  seasons: ISeason[];
+  type: string;
+}
+
+export interface IEpisode {
+  id: 2558114;
+  name: string;
+  overview: string;
+  vote_average: number;
+  vote_count: number;
+  air_date: string;
+  episode_number: number;
+  episode_type: string;
+  production_code: string;
+  runtime: number;
+  season_number: number;
+  show_id: number;
+  still_path: string;
+}
+
+export interface ISeason {
+  air_date: string;
+  episode_count: number;
+  id: number;
+  name: string;
+  overview: string;
+  poster_path: string;
+  season_number: number;
+  vote_average: number;
+}
