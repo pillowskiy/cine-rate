b640c8278961ca471120c47999623839c662c16d
Pillow <fagodfagod@gmail.com>
2023-09-06 17:32:12 +0300

    movies -> tagline for creation overview

--- app/_components/creation/creation-overview.tsx
+++ app/_components/creation/creation-overview.tsx
@@ -6,10 +6,12 @@ import { getTitle } from './common/utils';
 export default function CreationOverview({ details }: CreationDetailsProps) {
   return (
     <section>
-      <div className='flex items-center justify-between'>
+      <div className='flex max-w-full items-center justify-between'>
         <div className='space-y-1'>
           <h2 className='text-2xl font-semibold tracking-tight'>About</h2>
-          <p className='text-sm text-muted-foreground'>About this creation.</p>
+          <p className='text-sm text-muted-foreground'>
+            {details.tagline || `About ${getTitle(details)}`}.
+          </p>
         </div>
       </div>
       <Separator className='my-4' />
