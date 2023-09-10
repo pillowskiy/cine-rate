afd28e282663e70742a6c9c613d8550f6d1227d9
Pillow <fagodfagod@gmail.com>
2023-09-10 15:54:13 +0300

    search -> add debounce for fetching

--- app/_components/dialog/search-dialog.tsx
+++ app/_components/dialog/search-dialog.tsx
@@ -1,16 +1,27 @@
 'use client';
 
 import { cn, groupBy } from '@libs/index';
-import { Dialog, DialogClose, DialogContent, DialogTrigger } from '@ui/dialog';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogTrigger,
+} from '@ui/dialog';
 import { Button } from '@ui/button';
-import { Loader, Search, X } from 'lucide-react';
-import { type ChangeEvent, useState } from 'react';
+import { Loader, Search, Star, X } from 'lucide-react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { MultiSearchResponse } from '@/app/_types/search-types';
 import { MediaType } from '@/app/_types';
+import { ImageFromPath } from '../image/image-from-path';
+import { buildImagePath } from '@/app/_libs/tmdb';
+import Link from 'next/link';
+import { Carousel } from '../carousel';
+import { PersonArticle } from '../article/person-article';
+import { useDebounce } from '@/app/_hooks/useDebounce';
 
 function getMultipleSearch(query: string) {
-  return axios.get<MultiSearchResponse>('api/search/multiple', {
+  return axios.get<MultiSearchResponse>('/api/search/multiple', {
     params: { query },
   });
 }
@@ -22,25 +33,22 @@ export function SearchDialog() {
     MultiSearchResponse['results']
   > | null>(null);
   const [query, setQuery] = useState('');
+  const debounceQuery = useDebounce(query);
 
-  const handleQuery = ({ target }: ChangeEvent<HTMLInputElement>) => {
-    setQuery(target.value.toLowerCase());
+  useEffect(() => {
     setIsLoading(true);
-    getMultipleSearch(query)
+    getMultipleSearch(debounceQuery)
       .then(({ data }) => {
-        console.time();
         setData(groupBy(data.results, 'media_type'));
-        console.timeEnd();
       })
-      .catch((err) => {
-        console.log(err.response);
+      .catch(() => {
         setData(null);
       })
       .finally(() => setIsLoading(false));
-  };
+  }, [debounceQuery]);
 
   return (
-    <Dialog>
+    <Dialog onOpenChange={(isOpen) => !isOpen && setData(null)}>
       <DialogTrigger asChild>
         <Button
           variant='outline'
@@ -48,18 +56,17 @@ export function SearchDialog() {
             'relative w-full justify-start text-sm text-muted-foreground sm:pr-12 md:w-40 lg:w-64'
           )}
         >
-          <span className='hidden lg:inline-flex'>Search documentation...</span>
+          <span className='hidden lg:inline-flex'>
+            Search for a creation...
+          </span>
           <span className='inline-flex lg:hidden'>Search...</span>
-          <kbd className='pointer-events-none absolute right-1.5 top-1.5 hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex'>
-            <span className='text-xs'>⌘</span>K
-          </kbd>
         </Button>
       </DialogTrigger>
       <DialogContent className='gap-0 p-0'>
         <div className='mt-1 flex w-full items-center justify-between gap-2 border-b px-2 pb-1'>
           <Search className='h-4 w-4 text-foreground/70' />
           <input
-            onChange={handleQuery}
+            onChange={({ target }) => setQuery(target.value.toLowerCase())}
             className='flex-grow border-none px-0 py-2 text-sm outline-none'
             placeholder='Type a command or search...'
           />
@@ -67,7 +74,7 @@ export function SearchDialog() {
             <X className='h-4 w-4 text-foreground/70 opacity-0' />
           </DialogClose>
         </div>
-        <div className='relative max-h-[400px] min-h-[400px] space-y-4 overflow-y-auto px-2'>
+        <div className='relative max-h-[90vh] space-y-4 overflow-y-auto px-2 sm:max-h-[400px] sm:min-h-[400px]'>
           {isLoading && (
             <div className='absolute left-0 top-0 grid h-full w-full place-items-center bg-background'>
               <Loader className='m-auto h-16 w-16 animate-spin' />
