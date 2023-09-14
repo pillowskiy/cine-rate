01b69060409fdca5025b6e86acf4fbc19d8d19f4
Pillow <fagodfagod@gmail.com>
2023-09-14 19:03:13 +0300

    creation -> creation genres

---
+++ app/(routes)/api/genres/route.ts
@@ -0,0 +1,39 @@
+import { getGenres } from "@/app/_shared/actions/getGenres";
+import { MediaType } from "@/app/_types";
+import { isAxiosError } from "axios";
+import { NextRequest, NextResponse } from "next/server";
+import zod from 'zod';
+
+const queryDto = zod.object({
+    mediaType: zod.nativeEnum(MediaType),
+});
+
+export function GET(request: NextRequest) {
+    const requestUrl = new URL(request.url);
+    const searchParams = Object.fromEntries(requestUrl.searchParams.entries());
+    const result = queryDto.safeParse(searchParams);
+
+    if (!result.success) {
+        if (result.error.issues.length) {
+            return NextResponse.json(
+                { errors: result.error.format() },
+                { status: 400 }
+            );
+        }
+        return NextResponse.json(
+            { message: 'Unhandled zod error' },
+            { status: 500 }
+        );
+    }
+    const { mediaType, ...params } = result.data;
+    return getGenres(mediaType, params)
+        .then(({ data }) => {
+            return NextResponse.json(data, { status: 200 });
+        })
+        .catch((err) => {
+            if (!isAxiosError(err)) {
+                return NextResponse.json('Unhandled error occurred', { status: 500 });
+            }
+            return NextResponse.json(err.response?.data, { status: err.status });
+        });
+}
---
+++ app/_components/dialog/creation-filter-dialog/creation-genres.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import { useContext, useEffect, useState } from 'react';
+import type { GenresResponse } from '@app/types/genre-types';
+import type { MediaType } from '@app/types/index';
+import { Toggle } from '@ui/toggle';
+import { Label } from '@ui/label';
+import { Loader } from 'lucide-react';
+import { FilterContext } from '.';
+import axios from 'axios';
+
+interface CreationGenresProps {
+  mediaType: MediaType;
+}
+
+export function CreationGenres({ mediaType }: CreationGenresProps) {
+  const [genres, setGenres] = useState<GenresResponse['genres'] | null>(null);
+  const [filter, setFilter] = useContext(FilterContext);
+  const { with_genres } = filter;
+  const genreIds = with_genres?.split(',').map(Number) || [];
+
+  useEffect(() => {
+    axios
+      .get<GenresResponse>('/api/genres/', { params: { mediaType } })
+      .then(({ data }) => {
+        setGenres(data.genres);
+      })
+      .catch(() => {
+        setGenres([]);
+      });
+  }, [mediaType]);
+
+  if (!genres) {
+    return (
+      <div className='grid place-items-center'>
+        <Loader className='h-8 w-8' />
+      </div>
+    );
+  }
+
+  if (!genres.length) return null;
+
+  const removeGenre = (genreId: number) => {
+    setFilter((prev) => ({
+      ...prev,
+      with_genres: genreIds.filter((id) => genreId !== id).toString(),
+    }));
+  };
+
+  const addGenre = (genreId: number) => {
+    setFilter((prev) => ({
+      ...prev,
+      with_genres: genreIds.concat(genreId).toString(),
+    }));
+  };
+
+  return (
+    <div className='grid w-full items-center gap-2'>
+      <Label>Genres</Label>
+      <div className='flex flex-wrap gap-2'>
+        {genres.map((genre) => (
+          <Toggle
+            pressed={genreIds.includes(genre.id)}
+            onPressedChange={(pressed) =>
+              pressed ? addGenre(genre.id) : removeGenre(genre.id)
+            }
+            className='h-7 border'
+            key={genre.id}
+            size='sm'
+          >
+            {genre.name}
+          </Toggle>
+        ))}
+      </div>
+    </div>
+  );
+}
