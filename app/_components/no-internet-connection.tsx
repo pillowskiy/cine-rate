80952f78e0d89c8e60bb1dc82dce3570cf5f5d36
Pillow <fagodfagod@gmail.com>
2023-12-29 20:49:36 +0200

    fix: prevent multiple event listener

--- app/_components/no-internet-connection.tsx
+++ app/_components/no-internet-connection.tsx
@@ -2,28 +2,28 @@
 
 import { WifiOff } from 'lucide-react';
 import { useEffect, useState } from 'react';
-import { useClientReady } from '@hooks/useClientReady';
 
 export default function NoInternetConnection() {
-  const [online, setOnline] = useState(false);
-  const isClientReady = useClientReady();
+  const [online, setOnline] = useState(true);
+
   useEffect(() => {
     setOnline(navigator.onLine);
+    
+    const onlineHandler = () => setOnline(true);
+    const offlineHandler = () => setOnline(false);
+
+    window.addEventListener('online', onlineHandler);
+    window.addEventListener('offline', offlineHandler);
+
+    return () => {
+      window.removeEventListener('online', onlineHandler);
+      window.removeEventListener('offline', offlineHandler);
+    };
   }, []);
-
-  if (!isClientReady) return null;
-
-  window.addEventListener('online', () => {
-    setOnline(true);
-  });
-
-  window.addEventListener('offline', () => {
-    setOnline(false);
-  });
 
   if (!online) {
     return (
-      <div className='fixed top-0 z-50 select-none flex h-full w-full flex-col items-center justify-center backdrop-blur-xl bg-black/70 opacity-80'>
+      <div className='fixed top-0 z-50 flex h-full w-full select-none flex-col items-center justify-center bg-black/70 opacity-80 backdrop-blur-xl'>
         <WifiOff className='h-[48px] w-[48px] animate-pulse' />
         <span className='animate-pulse'>No internet connection.</span>
       </div>
