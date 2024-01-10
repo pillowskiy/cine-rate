c89b04e7de72e0b622bc78f347e9c22bd6c820b2
Pillow <fagodfagod@gmail.com>
2024-01-11 00:05:20 +0200

    chore -> transition to useSyncExternalStore

--- app/_components/no-internet-connection.tsx
+++ app/_components/no-internet-connection.tsx
@@ -1,27 +1,25 @@
 'use client';
 
 import { WifiOff } from 'lucide-react';
-import { useEffect, useState } from 'react';
+import { useSyncExternalStore } from 'react';
+
+function getSnapshot() {
+  return navigator.onLine;
+}
+
+function subscribe(callback: () => void) {
+  window.addEventListener('online', callback);
+  window.addEventListener('offline', callback);
+  return () => {
+    window.removeEventListener('online', callback);
+    window.removeEventListener('offline', callback);
+  }
+}
 
 export default function NoInternetConnection() {
-  const [online, setOnline] = useState(true);
+  const isOnline = useSyncExternalStore(subscribe, getSnapshot);
 
-  useEffect(() => {
-    setOnline(navigator.onLine);
-    
-    const onlineHandler = () => setOnline(true);
-    const offlineHandler = () => setOnline(false);
-
-    window.addEventListener('online', onlineHandler);
-    window.addEventListener('offline', offlineHandler);
-
-    return () => {
-      window.removeEventListener('online', onlineHandler);
-      window.removeEventListener('offline', offlineHandler);
-    };
-  }, []);
-
-  if (!online) {
+  if (!isOnline) {
     return (
       <div className='fixed top-0 z-50 flex h-full w-full select-none flex-col items-center justify-center bg-black/70 opacity-80 backdrop-blur-xl'>
         <WifiOff className='h-[48px] w-[48px] animate-pulse' />
