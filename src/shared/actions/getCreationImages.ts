20bed842b2fb504316a8848123915203901514f2
PillowPowa <fagodfagod@gmail.com>
2024-08-01 20:27:07 +0300

    fix(locale): language independent images

--- src/shared/actions/getCreationImages.ts
+++ src/shared/actions/getCreationImages.ts
@@ -8,8 +8,12 @@ export function getCreationImages(
   mediaType: MediaType,
   params?: BaseParams
 ) {
+  // TEMP: Empty string is required, because api interceptor set language automatically
+  // and tmdb api result depends on it, so we have to remove language from query
+  // also we can use include_image_language query, but i wanna be sure, that user gets all known images
+  (params ||= {}).language = '';
   return $api.safeFetch<CreationImagesResponse>(
     `/${mediaType}/${creationId}/images`,
-    { params }
+    { params, next: { revalidate: 24 * 3600 } }
   );
 }
